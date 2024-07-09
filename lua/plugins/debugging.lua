3485bd25e90684330c1252175bb94dbb3b83b6e8
Grant Ramsay <seapagan@gmail.com>
2024-07-09 22:20:03 +0100

    add base config for debugging
    still need to add support for each language we want to debug
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,34 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    -- setup dap-ui and other debuggers here
+    require("dapui").setup()
+
+    -- add some listeners
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- finally some useful keymaps
+    vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
+}
