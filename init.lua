02a004f3e2764d863605d605f0e76351243a20e3
Grant Ramsay <seapagan@gmail.com>
2024-07-09 10:34:48 +0100

    change lazy.nvim setup
    hide the nitifications for detected changes
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 require("lua.core.options")
 require("lua.core.lazy")
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    change_detection = {
+      enabled = true,
+      notify = false,
+    },
+})
 require("lua.core.keymaps")
