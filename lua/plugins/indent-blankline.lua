dec948ceec378bad71e317411e15cd845fad9c5e
Grant Ramsay <seapagan@gmail.com>
2024-07-09 23:52:04 +0100

    add several more useful plugins
    barbecue, bigfile, copilot, indent-blankline, nvim-autopairs,
    rainbow-delimiters
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+-- Indentation guides
+return {
+  -- https://github.com/lukas-reineke/indent-blankline.nvim
+  "lukas-reineke/indent-blankline.nvim",
+  event = 'VeryLazy',
+  main = "ibl",
+  config = function()
+    require("ibl").setup({})
+  end,
+
+
+
+}
