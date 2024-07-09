39cbee791da396248c9498b2cac03fc69bafd0f7
Grant Ramsay <seapagan@gmail.com>
2024-07-09 10:34:12 +0100

    add some useful options
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,15 @@
+-- global options for our neovim config
 local opt = vim.opt
+
+-- line numbering
+opt.number = true
+opt.relativenumber = true
+
+-- Show ruler at 80 chars
+opt.colorcolumn = "80"
+
+-- Cursor Line
+opt.cursorline = true
 
 -- Tabs & Indentation
 opt.tabstop = 2
@@ -16,4 +27,8 @@ vim.api.nvim_command([[autocmd FileType python set tabstop=4]])
 vim.api.nvim_command([[autocmd FileType python set shiftwidth=4]])
 vim.api.nvim_command([[autocmd FileType python set expandtab]])
 
-
+-- Misc appearance settings
+opt.signcolumn = "yes"
+vim.diagnostic.config {
+  float = { border = "rounded" }, -- add border to diagnostic popups
+}
