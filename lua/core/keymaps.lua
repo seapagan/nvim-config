125ac09235f26571c4c6cc453f957c0a9a47435b
Grant Ramsay <seapagan@gmail.com>
2024-07-10 11:29:23 +0100

    add more useful general keymaps
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,26 @@
--- put your custom keymaps here and they will be imported 
--- after the plugins have been loaded
+-- Define some keymaps that are not specific to any plugin
+local keymap = vim.keymap
+
+-- General keymaps
+keymap.set("i", "jk", "<ESC>") -- exit insert mode with jk 
+keymap.set("i", "ii", "<ESC>") -- exit insert mode with ii
+keymap.set("n", "<leader>wq", ":wq<CR>") -- save and quit
+keymap.set("n", "<leader>qq", ":q!<CR>") -- quit without saving
+keymap.set("n", "<leader>ww", ":w<CR>") -- save
+keymap.set("n", "gx", ":!open <c-r><c-a><CR>") -- open URL under cursor
+
+-- Split window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
+keymap.set("n", "<leader>sj", "<C-w>-") -- make split window height shorter
+keymap.set("n", "<leader>sk", "<C-w>+") -- make split windows height taller
+keymap.set("n", "<leader>sl", "<C-w>>5") -- make split windows width bigger 
+keymap.set("n", "<leader>sh", "<C-w><5") -- make split windows width smaller
+
+-- Tab management
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
