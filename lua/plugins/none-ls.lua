d529368caa482a8a6741b845864542856a19834f
Grant Ramsay <seapagan@gmail.com>
2024-07-11 11:19:02 +0100

    fix mypy error on new file
    if a python file is created inside nvim, mypy gives an error
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,33 @@ return {
         null_ls.builtins.formatting.prettierd,
 
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.mypy,
+        -- null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with {
+          -- see issue https://github.com/nvimtools/none-ls.nvim/issues/97
+          args = function(params)
+            return {
+              "--hide-error-codes",
+              "--hide-error-context",
+              "--no-color-output",
+              "--show-absolute-path",
+              "--show-column-numbers",
+              "--show-error-codes",
+              "--no-error-summary",
+              "--no-pretty",
+              params.temp_path,
+            }
+          end,
+          on_output = function(line, params)
+            line = line:gsub(
+              params.temp_path:gsub("([^%w])", "%%%1"),
+              params.bufname
+            )
+            return null_ls.builtins.diagnostics.mypy._opts.on_output(
+              line,
+              params
+            )
+          end,
+        },
       },
     }
 
