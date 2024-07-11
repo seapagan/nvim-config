2cd1c3a0286fee486a2fb372d28cc29a74113f6b
Grant Ramsay <seapagan@gmail.com>
2024-07-11 11:11:57 +0100

    remove ruff from none_ls config
    this seems to be crashing none_ls so than no diag/formatter after this
    is being loaded
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.formatting.ruff,
         null_ls.builtins.formatting.prettier.with {
           filetypes = {
             "html",
