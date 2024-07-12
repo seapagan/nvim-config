729cefe3eb7c62a75f7b5d1dc13213bdd8ece8c2
Grant Ramsay <seapagan@gmail.com>
2024-07-12 07:50:51 +0100

    tweak lazy.vim settings
    check for plugin updates with notifications, and add a rounded border
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- init.lua
+++ init.lua
@@ -1,14 +1,22 @@
 -- import our global options
-require("core.options")
+require "core.options"
 
 -- set up 'lazy.nvim' package manager
-require("core.lazy-nvim")
+require "core.lazy-nvim"
 require("lazy").setup("plugins", {
-    change_detection = {
-      enabled = true,
-      notify = false,
-    },
+  change_detection = {
+    enabled = true,
+    notify = true,
+  },
+  checker = {
+    enabled = true,
+    interval = 3600, -- check every hour
+    notify = true,
+  },
+  ui = {
+    border = "rounded",
+  }
 })
 
 -- import any custom keymaps
-require("core.keymaps")
+require "core.keymaps"
