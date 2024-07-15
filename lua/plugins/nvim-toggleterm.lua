f0f688ed323029808457b0269c39320b02b1c0dc
Grant Ramsay <seapagan@gmail.com>
2024-07-15 10:25:31 +0100

    modify toggle-term to be a floating window in middle of screen
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/plugins/nvim-toggleterm.lua
+++ lua/plugins/nvim-toggleterm.lua
@@ -1,11 +1,19 @@
 return {
-  'akinsho/nvim-toggleterm.lua', 
-  version='*', 
+  "akinsho/nvim-toggleterm.lua",
+  version = "*",
   config = function()
-    require("toggleterm").setup{
-      vsize = 20,
+    require("toggleterm").setup {
+      vsize = 10,
       open_mapping = [[<c-\>]],
-      direction = 'horizontal',
+      -- direction = 'horizontal',
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
     }
-  end
+  end,
 }
