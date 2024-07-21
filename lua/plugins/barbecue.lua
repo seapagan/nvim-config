cb0c718aa38f7ab0e832d8417b2733ac6facc8b1
Grant Ramsay <seapagan@gmail.com>
2024-07-21 18:50:21 +0100

    add some buffer delete keymaps
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -15,5 +15,8 @@ return {
     -- keymaps
     vim.keymap.set("n", "<Leader>bp", ":bprevious<CR>", { silent = true, noremap = true })
     vim.keymap.set("n", "<Leader>bn", ":bnext<CR>", { silent = true, noremap = true })
+    vim.keymap.set("n", "<Leader>bd", ":bdelete<CR>", { silent = true, noremap = true })
+    -- delete all buffers except the current one
+    vim.keymap.set("n", "<Leader>bD", ":%bd<bar>e#<bar>bd#<CR><bar>'\"", { silent = true, noremap = true })
   end
 }
